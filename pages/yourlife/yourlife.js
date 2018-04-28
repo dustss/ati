371e582703f98ac5af04072c491671885e97e65b
dustss <oneisoneonlyone@foxmail.com>
2018-04-28 12:56:42 +0530

    列表+圆点涂色

--- pages/yourlife/yourlife.js
+++ pages/yourlife/yourlife.js
@@ -8,6 +8,7 @@ Page({
     pen:-1
   },
   onLoad: function (){
+    //初始化绘图工具
     let brushes=[]
     for(let i=0;i<5;i++){
       brushes.push({
@@ -17,9 +18,9 @@ Page({
     this.setData({
       brushes: brushes
     })
-    
+    // 初始化列表条目
     let half=[]
-    for(let i=0;i<24;i++){
+    for(let i=0;i<12;i++){
       half.push({
         paintColor: 'gainsboro'
       })
@@ -29,7 +30,9 @@ Page({
     })
 
   },
-
+/**
+ * 点击列表单个条目 变换条目颜色
+ */
   onPainting(e){  
       var id = e.currentTarget.id;
       if(this.data.pen==-1) {
@@ -44,6 +47,9 @@ Page({
 //   onRealPainting(e){
 // console.log(e)
 //   },
+/**
+ * 绘图颜色 绘图工具选择
+ */
   onPenSelected(f){
     var pen = f.currentTarget.id;
     if(this.data.pen==pen)
@@ -55,5 +61,13 @@ Page({
         pen: pen
       })
     }
+  },
+/**
+ * 模拟涂抹绘图效果
+ */
+  onTouchMoving(e){
+    console.log(e)
+
   }
+
 })
