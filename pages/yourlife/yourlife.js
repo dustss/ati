04097e6a48d3c088d39db28e62d5816cd0221a16
dustss <oneisoneonlyone@foxmail.com>
2018-04-29 22:50:51 +0530

    二维平面+点按方式涂色

--- pages/yourlife/yourlife.js
+++ pages/yourlife/yourlife.js
@@ -3,16 +3,15 @@ const COLUMN_NUMBER = 7
 
 Page({
   data: {
-    grid: [],
-    row: Array.from(new Array(5),(val,index)=>index),
+    pixels: [],
     brushes: [],
-    brushColor: ['tomato', 'orange', 'greenyellow', 'skyblue','pink'],
-    pen:-1
+    brushColor: ['tomato', 'orange', 'greenyellow', 'skyblue', 'pink'],
+    pen: -1
   },
-  onLoad: function (){
+  onLoad: function () {
     //初始化绘图工具
-    let brushes=[]
-    for(let i=0;i<5;i++){
+    let brushes = []
+    for (let i = 0; i < 5; i++) {
       brushes.push({
         brushColor: this.data.brushColor[i]
       })
@@ -20,51 +19,53 @@ Page({
     this.setData({
       brushes: brushes
     })
-    // 初始化列表条目
-    let grid=[];
-    for (let i = 0; i < COLUMN_NUMBER;i++)
-    {
-      grid.push(this.data.row)
+
+    //初始化 行列
+    let grid = []
+    for (let i = 0; i < 7; i++) {
+      let row = grid[i] = []
+      for (let j = 0; j < 5; j++) {
+        row.push({
+          paintColor: "gainsboro"
+        })
+      }
     }
     this.setData({
-      grid:grid,
+      grid: grid
     })
-
 
   },
 
-/**
- * 点击列表单个条目 变换条目颜色
- */
-  onPainting(e){  
-    console.log(e.currentTarget.id)
-    var x= e.currentTarget.id[0]
+  /**
+   * 点击单个pixel 变换颜色
+   */
+  onPainting(e) {
+    var x = e.currentTarget.id[0]
     var y = e.currentTarget.id[2]
-    
+    this.data.grid[x][y].paintColor = this.data.brushColor[this.data.pen]
+
+    this.setData({
+      grid: this.data.grid
+    })
 
   },
 
-/**
- * 绘图颜色 绘图工具选择
- */
-  onPenSelected(f){
+  /**
+   * 绘图颜色 绘图工具选择
+   */
+  onPenSelected(f) {
     var pen = f.currentTarget.id;
-    if(this.data.pen==pen)
-    this.setData({
-      pen: -1
-    })
-    else{
+    if (this.data.pen == pen)
+      this.setData({
+        pen: -1
+      })
+    else {
       this.setData({
         pen: pen
       })
     }
   },
 
-/**
- * 模拟涂抹绘图效果
- */
-  onTouchMoving(e){
-    console.log(e)
-  }
+
 
 })
